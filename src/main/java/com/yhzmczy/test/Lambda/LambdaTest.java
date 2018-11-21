77b7cc532477af93c70165b399cc90d132a48ddf
yhzmczy <576529339@qq.com>
2018-11-21 14:01:09 +0800

    删除target

--- src/main/java/com/yhzmczy/test/Lambda/LambdaTest.java
+++ src/main/java/com/yhzmczy/test/Lambda/LambdaTest.java
@@ -3,11 +3,10 @@ package com.yhzmczy.test.Lambda;
 import java.util.function.Consumer;
 
 public class LambdaTest {
+    
     public static void main(String[] args) {
         Runnable r = () -> System.out.println("Hello World!");
-        Runnable r2 = {
-
-        };
+        Runnable r2 = () -> System.out.println("Hello World!2");
         Thread th = new Thread(r);
         th.start();
     }
